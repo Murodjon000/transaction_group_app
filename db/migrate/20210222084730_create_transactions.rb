3ccdd71e9805eccf16030942487d727609268ee9
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-02-22 21:09:50 +0500

    add transactions

---
+++ db/migrate/20210222084730_create_transactions.rb
@@ -0,0 +1,11 @@
+class CreateTransactions < ActiveRecord::Migration[6.0]
+  def change
+    create_table :transactions do |t|
+      t.string :name
+      t.integer :amount
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
