a2de2e81d01a681c957ef7248e57cae9d17d616a
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-02-23 22:38:26 +0500

    add group model spec test

---
+++ spec/models/group_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Group, type: :model do
+  subject(:group) { build(:group) }
+
+  describe 'associations' do
+    it { should belong_to(:user) }
+    it { should have_many(:transactions) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name)}
+    it { should validate_presence_of(:icon)}
+  end
+end
