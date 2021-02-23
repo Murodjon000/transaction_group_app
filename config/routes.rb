951bf46cf56e067782d5e7630dcb901f4cc84266
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-02-23 22:35:42 +0500

    add external transaction route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root 'user#show'
+  get 'external_transaction', to: 'transactions#external_transaction'
+  resources :user, only: [:show]
+  resources :transactions, except: [:show]
+  resources :groups, except: [:destroy, :update]
 end
