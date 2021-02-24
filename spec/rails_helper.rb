c756e2014fc1695cf601e5c0709cedfab8aa6eeb
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-02-24 16:16:12 +0500

    add request sign in

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -38,8 +38,8 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
-  # config.include Features::SessionsHelper
-  # config.include Devise::Test::IntegrationHelpers, type: :request
+  config.include Features::SessionsHelper
+   config.include Devise::Test::IntegrationHelpers, type: :request
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
 
