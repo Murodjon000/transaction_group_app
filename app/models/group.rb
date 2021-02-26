20fff8b24c935545620c1c79ea14bb648501604a
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-02-26 14:51:23 +0500

    add payment groups

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,7 @@
 class Group < ApplicationRecord
   belongs_to :user
-  has_many :transactions, dependent: :destroy
+  has_many :payments_groups, dependent: :destroy
+  has_many :payments, through: :payments_groups
 
   validates :name, presence: true
   validates :icon, presence: true
